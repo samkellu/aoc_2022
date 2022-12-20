3843acc0cd4585a38414b8f5040b11a719e58d2a
sam <samkelly1290@gmail.com>
2022-12-20 18:05:31 +1100

    inefficient but it works. day 12 complete

--- day12/day12p1.c
+++ day12/day12p1.c
@@ -2,78 +2,133 @@
 #include <stdlib.h>
 #include <string.h>
 
-void find_path(char** map, int cur[2]) {
+#define WIDTH 163
+#define HEIGHT 41
 
-    printf("call\n");
-    if (map[cur[0]][cur[1]] == 'E') {
-        printf("done");
-    }
-
-    int next[2];
-
-    if (map[cur[0]][cur[1]+1] == map[cur[0]][cur[1]] + 1 || map[cur[0]][cur[1]+1] == map[cur[0]][cur[1]]
-         || map[cur[0]][cur[1]+1] == map[cur[0]][cur[1]] - 1 || map[cur[0]][cur[1]+1] == 'E') {
-
-        next[0] = cur[0];
-        next[1] = cur[1]+1;
-        find_path(map, next);
-    }
-    if (map[cur[0]][cur[1]-1] == map[cur[0]][cur[1]] + 1 || map[cur[0]][cur[1]-1] == map[cur[0]][cur[1]]
-         || map[cur[0]][cur[1]-1] == map[cur[0]][cur[1]] - 1 || map[cur[0]][cur[1]-1] == 'E') {
-
-        next[0] = cur[0];
-        next[1] = cur[1]-1;
-        find_path(map, next);
-            
-    }
-    if (map[cur[0]+1][cur[1]] == map[cur[0]][cur[1]] + 1 || map[cur[0]+1][cur[1]] == map[cur[0]][cur[1]]
-         || map[cur[0]+1][cur[1]] == map[cur[0]][cur[1]] - 1 || map[cur[0]+1][cur[1]] == 'E') {
-
-        next[0] = cur[0]+1;
-        next[1] = cur[1];
-        find_path(map, next);
-    }
-    if (map[cur[0]-1][cur[1]] == map[cur[0]][cur[1]] + 1 || map[cur[0]-1][cur[1]] == map[cur[0]][cur[1]]
-         || map[cur[0]-1][cur[1]] == map[cur[0]][cur[1]] - 1 || map[cur[0]-1][cur[1]] == 'E') {
-
-        next[0] = cur[0]-1;
-        next[1] = cur[1];
-        find_path(map, next);
-    }
-}
 
 int main() {
 
     FILE* fp = fopen("day12.in", "r");
     char buf[512];
-
-    char** map = malloc(0);
+    char map[HEIGHT][WIDTH];
     int num_lines = 0;
 
     while (fgets(buf, 512, fp)) {
-        map = realloc(map, sizeof(char*) * ++num_lines);
-        map[num_lines-1] = malloc(sizeof(char) * strlen(buf));
-        memcpy(map[num_lines-1], buf, strlen(buf));
+        
+        memcpy(map[num_lines++], buf, strlen(buf));
     }
 
-    int start[2];
+    int** queue = malloc(sizeof(int*)*2);
+    queue[0] = malloc(sizeof(int)*2);
+    queue[1] = malloc(sizeof(int)*2);
+    queue[1][0] = -1;
+    int queue_size = 2;
+
     int end[2];
 
-    for (int i = 0; i < num_lines; i++) {
-        for (int j = 0; j < strlen(map[i]); j++) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
             if (map[i][j] == 'S') {
-                start[0] = i;
-                start[1] = j;
-            } else if (map[i][j] == 'E') {
+                queue[0][0] = i;
+                queue[0][1] = j;
+                map[i][j] = 'a';
+            }
+            if (map[i][j] == 'E') {
                 end[0] = i;
                 end[1] = j;
+                map[i][j] = 'z';
             }
         }
     }
 
-    find_path(map,start);
+    int vis[HEIGHT][WIDTH] = { 0 };
+    int cursor = 0;
+    int layer = 0;
+    int valid_layer = 0;
 
-    free(map);
+    while (cursor < queue_size) {
+
+        if (queue[cursor][0] == -1) {
+            printf("layer %d\n", layer);
+            free(queue[cursor]);
+            cursor++;
+            queue = realloc(queue, sizeof(int*) * ++queue_size);
+            if (!valid_layer) {
+                break;
+            }
+            queue[queue_size-1] = malloc(sizeof(int)*2);
+            queue[queue_size-1][0] = -1;
+            valid_layer = 0;
+            layer++;
+            continue;
+        }
+
+        int val = map[queue[cursor][0]][queue[cursor][1]];
+        if (queue[cursor][0] == end[0] && queue[cursor][1] == end[1]) {
+            printf("done %d\n\n\n\n", layer);
+            break;
+        }
+
+        vis[queue[cursor][0]][queue[cursor][1]] = 1;
+
+        int num_offset = 0;
+        int** offset = malloc(0);
+
+        if (queue[cursor][0] > 0) {
+            offset = realloc(offset, sizeof(int*) * ++num_offset);
+            offset[num_offset-1] = malloc(sizeof(int) * 2);
+            offset[num_offset-1][0] = -1; 
+            offset[num_offset-1][1] = 0; 
+        }
+        if (queue[cursor][0] + 1 < HEIGHT) {
+            offset = realloc(offset, sizeof(int*) * ++num_offset);
+            offset[num_offset-1] = malloc(sizeof(int) * 2);
+            offset[num_offset-1][0] = 1; 
+            offset[num_offset-1][1] = 0; 
+        }
+        if (queue[cursor][1] + 1 < WIDTH) {
+            offset = realloc(offset, sizeof(int*) * ++num_offset);
+            offset[num_offset-1] = malloc(sizeof(int) * 2);
+            offset[num_offset-1][0] = 0; 
+            offset[num_offset-1][1] = 1; 
+        }
+        if (queue[cursor][1] > 0) {
+            offset = realloc(offset, sizeof(int*) * ++num_offset);
+            offset[num_offset-1] = malloc(sizeof(int) * 2);
+            offset[num_offset-1][0] = 0; 
+            offset[num_offset-1][1] = -1; 
+        }
+
+        for (int i = 0; i < num_offset; i++) {
+            if (vis[queue[cursor][0] + offset[i][0]][queue[cursor][1] + offset[i][1]] != 1 && 
+               map[queue[cursor][0] + offset[i][0]][queue[cursor][1] + offset[i][1]] <= val + 1) {
+
+                printf("%d %d\n", queue[cursor][0] + offset[i][0], queue[cursor][1] + offset[i][1]);
+                for (int j = cursor+1; j < queue_size; j++) {
+                    if (queue[cursor][0] == queue[j][0] && queue[cursor][1] == queue[j][1]) {
+                        goto NEXT;
+                    }
+                }
+                queue = realloc(queue, sizeof(int*) * ++queue_size);
+                queue[queue_size-1] = malloc(sizeof(int)*2);
+                queue[queue_size-1][0] = queue[cursor][0] + offset[i][0];
+                queue[queue_size-1][1] = queue[cursor][1] + offset[i][1];
+                valid_layer = 1;
+                NEXT:
+            }
+            free(offset[i]);
+        }
+        free(offset);
+        free(queue[cursor]);
+        cursor++;
+    }
+
+    if (cursor != queue_size) {
+        for (int i = cursor; cursor < queue_size-1; i++) {
+            free(queue[i]);
+        }
+    }
+    free(queue);
     fclose(fp);
     return 1;
 }
---
+++ day12/day12p2.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define WIDTH 163
+#define HEIGHT 41
+
+
+int main() {
+
+    FILE* fp = fopen("day12.in", "r");
+    char buf[512];
+    char map[HEIGHT][WIDTH];
+    int num_lines = 0;
+
+    while (fgets(buf, 512, fp)) {
+        memcpy(map[num_lines++], buf, strlen(buf));
+    }
+    fclose(fp);
+
+    int** start_pos = malloc(0);
+    int num_start = 0;
+
+    int end[2];
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (map[i][j] == 'S') {
+                map[i][j] = 'a';
+            }
+            if (map[i][j] == 'a') {
+                start_pos = realloc(start_pos, sizeof(int*)*++num_start);
+                start_pos[num_start-1] = malloc(sizeof(int*)*2);
+                start_pos[num_start-1][0] = i;
+                start_pos[num_start-1][1] = j;
+            }
+            if (map[i][j] == 'E') {
+                end[0] = i;
+                end[1] = j;
+                map[i][j] = 'z';
+            }
+        }
+    }
+
+    int start_cursor = 0;
+    int min = -1;
+
+    while (start_cursor < num_start) {
+
+        int** queue = malloc(sizeof(int*)*2);
+        queue[0] = malloc(sizeof(int)*2);
+        queue[0][0] = start_pos[start_cursor][0];
+        queue[0][1] = start_pos[start_cursor++][1];
+        queue[1] = malloc(sizeof(int)*2);
+        queue[1][0] = -1;
+        int queue_size = 2;
+
+        int vis[HEIGHT][WIDTH] = { 0 };
+        int cursor = 0;
+        int layer = 0;
+        int valid_layer = 0;
+
+        while (cursor < queue_size) {
+            if (queue[cursor][0] == -1) {
+
+                free(queue[cursor]);
+                cursor++;
+                queue = realloc(queue, sizeof(int*) * ++queue_size);
+                if (!valid_layer) {
+                    break;
+                }
+                queue[queue_size-1] = malloc(sizeof(int)*2);
+                queue[queue_size-1][0] = -1;
+                valid_layer = 0;
+                layer++;
+                continue;
+            }
+
+            int val = map[queue[cursor][0]][queue[cursor][1]];
+            if (queue[cursor][0] == end[0] && queue[cursor][1] == end[1]) {
+                printf("done %d\n", layer);
+                min = (layer < min || min == -1) ? layer : min;
+                // break;
+            }
+
+            vis[queue[cursor][0]][queue[cursor][1]] = 1;
+
+            int num_offset = 0;
+            int** offset = malloc(0);
+
+            if (queue[cursor][0] > 0) {
+                offset = realloc(offset, sizeof(int*) * ++num_offset);
+                offset[num_offset-1] = malloc(sizeof(int) * 2);
+                offset[num_offset-1][0] = -1; 
+                offset[num_offset-1][1] = 0; 
+            }
+            if (queue[cursor][0] + 1 < HEIGHT) {
+                offset = realloc(offset, sizeof(int*) * ++num_offset);
+                offset[num_offset-1] = malloc(sizeof(int) * 2);
+                offset[num_offset-1][0] = 1; 
+                offset[num_offset-1][1] = 0; 
+            }
+            if (queue[cursor][1] + 1 < WIDTH) {
+                offset = realloc(offset, sizeof(int*) * ++num_offset);
+                offset[num_offset-1] = malloc(sizeof(int) * 2);
+                offset[num_offset-1][0] = 0; 
+                offset[num_offset-1][1] = 1; 
+            }
+            if (queue[cursor][1] > 0) {
+                offset = realloc(offset, sizeof(int*) * ++num_offset);
+                offset[num_offset-1] = malloc(sizeof(int) * 2);
+                offset[num_offset-1][0] = 0; 
+                offset[num_offset-1][1] = -1; 
+            }
+
+            for (int i = 0; i < num_offset; i++) {
+                if (vis[queue[cursor][0] + offset[i][0]][queue[cursor][1] + offset[i][1]] != 1 && 
+                map[queue[cursor][0] + offset[i][0]][queue[cursor][1] + offset[i][1]] <= val + 1) {
+
+                    for (int j = cursor+1; j < queue_size; j++) {
+                        if (queue[cursor][0] == queue[j][0] && queue[cursor][1] == queue[j][1]) {
+                            goto NEXT;
+                        }
+                    }
+                    queue = realloc(queue, sizeof(int*) * ++queue_size);
+                    queue[queue_size-1] = malloc(sizeof(int)*2);
+                    queue[queue_size-1][0] = queue[cursor][0] + offset[i][0];
+                    queue[queue_size-1][1] = queue[cursor][1] + offset[i][1];
+                    valid_layer = 1;
+                    NEXT:
+                }
+                free(offset[i]);
+            }
+            free(offset);
+            free(queue[cursor]);
+            cursor++;
+        }
+
+        free(start_pos[start_cursor-1]);
+        free(queue);
+    }
+    printf("Minimum: %d\n", min);
+    free(start_pos);
+    return 1;
+}
